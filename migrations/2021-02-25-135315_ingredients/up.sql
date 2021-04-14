10e6911905469f86e9d9b58e4fbd8841c34b5b89
tim.mulqueen <tim.mulqueen@gmail.com>
2021-04-14 23:16:35 +0200

    fix broken csv import

--- migrations/2021-02-25-135315_ingredients/up.sql
+++ migrations/2021-02-25-135315_ingredients/up.sql
@@ -6,5 +6,11 @@ CREATE TABLE ingredients (
     food_group TEXT,
     food_subgroup VARCHAR(250)
 );
-COPY ingredients
+COPY ingredients (
+    name,
+    name_scientific,
+    decription,
+    food_group,
+    food_subgroup
+)
 from '/var/lib/postgres/fixtures/ingredients.csv' DELIMITER ',' CSV HEADER;
